af517f43796f6e50e8b8cb73e9c74cf11485b88f
Martin Pärtlas <Martin.Partlas@tptlive.ee>
2021-12-07 21:20:15 +0200

    save/load system and basic main menu

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ HEIGHT = 700
 win = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Warframe crafter with imageAI")
 
-main_font = pygame.font.SysFont('comicsans', 35)
+main_font = pygame.font.SysFont('comicsans', 60)
 
 data = {"materials": {"alloyplate": 0, "argoncrystal": 0, "circuits": 0, "controlmodule": 0, "cryotic": 0, "ferrite": 0, "gallium": 0, 
         "hexenon": 0, "morphics": 0, "nanospores": 0, "neuralsensors": 0, "neurodes": 0, "orokincell": 0, "oxium": 0, "plastids": 0, 
@@ -22,19 +22,38 @@ data = {"materials": {"alloyplate": 0, "argoncrystal": 0, "circuits": 0, "contro
         "parts": {"barrel": 0, "forma": 0, "receiver": 0, "stock": 0, "excalibursystems": 0, "excaliburneuroptics": 0, 
         "excaliburchassis": 0}, "warframes": {"excalibur": 0}}
 
+mainMenu1 = main_font.render("INVENTORY", 1, (255, 255, 255))
+mainMenu2 = main_font.render("RESEARCH", 1, (255, 255, 255))
+mainMenu3 = main_font.render("CRAFT", 1, (255, 255, 255))
+mainMenu4 = main_font.render("SCAN", 1, (255, 255, 255))
+    
+
 def refresh():
-    pass
+    win.fill((0, 0, 0))
+    win.blit(mainMenu1, (50, 25))
+    win.blit(mainMenu2, (50, 25 + mainMenu1.get_height() + 25))
+    win.blit(mainMenu3, (50, 25 + mainMenu1.get_height() + mainMenu2.get_height() + 50))
+    win.blit(mainMenu4, (50, 25 + mainMenu1.get_height() + mainMenu2.get_height() + mainMenu3.get_height() + 75))
+
 
 def SaveLoadGame(load):
+    global data
     if load:
-        
+        if os.path.exists("savegames/save.json"):
+            with open("savegames/save.json", 'r') as save:
+                data = json.load(save)
+    else:
+        with open("savegames/save.json", 'w') as save:
+            json.dump(data, save)
 
 def game():
+    SaveLoadGame(True)
     run = True
     while run:
         refresh()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                SaveLoadGame(False)
                 run = False
         pygame.display.update()
 
